59898d7ece128b010f3e79fa6a2ded96b4bb8cee
atroian <andrii.troian@corezoid.com>
2022-04-05 14:43:04 +0300

    create service for post user data in db.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
-    TasksModule
+    TasksModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],
