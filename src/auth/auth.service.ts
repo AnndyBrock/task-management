cbfdd17269d29ab9d793561f726edeae3d0d58b1
atroian <andrii.troian@corezoid.com>
2022-04-05 23:21:25 +0300

    signin implementation jwt implementation and integrate token to task module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
-import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
-    private userRepository: UserRepository
+    private userRepository: UserRepository,
+    private jwtService: JwtService
   ){}
 
   signUp(authCredentialsDto: AuthCredentialsDto): Promise<void>{
     return this.userRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{accessToken: string}> {
+    const { user_name, password, email } = authCredentialsDto;
+    const user = await this.userRepository.findOne({ user_name });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload: JwtPayload = {user_name, email};
+      const accessToken: string = await this.jwtService.sign(payload);
+      return {accessToken};
+    } else {
+      throw new UnauthorizedException('')
+    }
   }
 }
