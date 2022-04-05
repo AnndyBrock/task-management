3e6263042aada398f55331e6b98ca726086e0759
atroian <andrii.troian@corezoid.com>
2022-04-05 20:16:09 +0300

    add hashing password before store in database

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,13 +2,17 @@ import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
 
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void>{
     const {user_name, password, email} = authCredentialsDto;
-    const user = this.create({ user_name, password, email });
+    //hash password
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(password, salt);
+    const user = this.create({ user_name, password: hashPassword, email });
     try {
       await this.save(user);
     } catch (e) {
